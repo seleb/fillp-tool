108250577b3c103a1f1c60c272264515f9a9d66e
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2018-07-09 00:30:10 -0400

    Initial commit

---
+++ src/components/app.jsx
@@ -0,0 +1,10 @@
+import { h, Component } from 'preact';
+
+export default class App extends Component {
+	render({ }, {
+	}) {
+		return (
+			<div class="app">
+		);
+	}
+}
---
+++ webpack.config.base.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const package = require('./package');
+
+module.exports = {
+	resolve: {
+		extensions: ['.js', '.jsx'],
+	},
+	module: {
+		rules: [{
+			test: /\.js(x)?$/, // source
+			exclude: /(node_modules|bower_components)/,
+			use: {
+				loader: 'babel-loader'
+			}
+		}, {
+			test: /\.strand$/,
+			use: "raw-loader"
+		}, {
+			test: /\.css$/, // stylesheets
+			use: [
+				'style-loader',
+				'css-loader',
+				{
+					loader: 'postcss-loader',
+					options: {
+						plugins: [
+							require('autoprefixer')(),
+							require('postcss-clean')()
+						]
+					}
+				}
+			]
+		}, {
+			test: /\.fnt\.(png||xml)$/, // bitmap fonts
+			use: {
+				loader: 'file-loader',
+				options: {
+					outputPath: 'assets/',
+					name: '[name].[ext]'
+				}
+			}
+		}, {
+			test: /(?<!\.fnt)\.(png|jpg|gif|svg|wav|ogg|mp3|frag|vert|xml|woff)$/, // assets, excluding bitmap fonts
+			use: {
+				loader: 'url-loader',
+				options: {
+					outputPath: 'assets/',
+					limit: Infinity // bytes
+				}
+			}
+		}]
+	},
+	entry: {
+		index: './src/index.js'
+	},
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'public')
+	},
+	plugins: [
+		new CleanWebpackPlugin(['public']), // cleans dist
+		new HtmlWebpackPlugin({ // creates index.html
+			title: package.description,
+			meta: {
+				viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no'
+			},
+			minify: true,
+			favicon: './src/assets/icon.png'
+		})
+	],
+};
