8e2e7485f7c800ce643735fc4c24066e6864368c
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2018-07-17 01:56:59 -0400

    show colour output

--- src/components/output.jsx
+++ src/components/output.jsx
@@ -24,16 +24,22 @@ export default class Output extends Component {
 	}
 	render({
 		bits = {},
+		colours: {
+			'0': c1,
+			'1': c2,
+		} = {},
 	}, {
 		copy = '',
 	}) {
 		const onBits = Object.entries(bits).filter(([_, on]) => on).map(([bit]) => bit);
 		const sum = onBits.reduce((sum, bit) => sum + parseInt(bit, 10), 0);
+		const coloursSum = c1+(c2<<4);
 		return (
 			<div class="output">
 				<label for="Hex">Hex Sum: </label><input name="Hex" onClick={this.copy} value={`0x${sum.toString(16)}`} />
 				<label for="Binary">Binary Sum: </label><input name="Binary" onClick={this.copy} value={`0b${sum.toString(2)}`} />
 				<label for="Decimal">Decimal Addition: </label><input name="Decimal" onClick={this.copy} value={onBits.join('+') || 0} />
+				<label for="color">Color: </label><input name="Color" onClick={this.copy} value={coloursSum} />
 				<div class="copy-notif">{copy}</div>
 			</div>
 		);
