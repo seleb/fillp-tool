649cd2b40fd61e991c94c8ae9844b6d36b820141
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-05-24 15:48:47 -0400

    fix: make output fields `readonly`

--- src/app/components/output.jsx
+++ src/app/components/output.jsx
@@ -37,15 +37,15 @@ export default class Output extends Component {
 		return (
 			<div class="output">
 				<label htmlFor="Hex">Hex Sum: </label>
-				<input id="Hex" name="Hex" onClick={this.copy} value={`0x${sum.toString(16)}`} />
+				<input readonly id="Hex" name="Hex" onClick={this.copy} value={`0x${sum.toString(16)}`} />
 				<label htmlFor="Binary">Binary Sum: </label>
-				<input id="Binary" name="Binary" onClick={this.copy} value={`0b${sum.toString(2)}`} />
+				<input readonly id="Binary" name="Binary" onClick={this.copy} value={`0b${sum.toString(2)}`} />
 				<label htmlFor="DecimalSum">Decimal Sum: </label>
-				<input id="DecimalSum" name="DecimalSum" onClick={this.copy} value={sum < 32768 ? sum.toString(10) : 'N/A over 32768'} />
+				<input readonly id="DecimalSum" name="DecimalSum" onClick={this.copy} value={sum < 32768 ? sum.toString(10) : 'N/A over 32768'} />
 				<label htmlFor="Decimal">Decimal Addition: </label>
-				<input id="Decimal" name="Decimal" onClick={this.copy} value={onBits.join('+') || 0} />
+				<input readonly id="Decimal" name="Decimal" onClick={this.copy} value={onBits.join('+') || 0} />
 				<label htmlFor="color">Color: </label>
-				<input id="color" name="Color" onClick={this.copy} value={coloursSum} />
+				<input readonly id="color" name="Color" onClick={this.copy} value={coloursSum} />
 				<div class="copy-notif">{copy}</div>
 			</div>
 		);
