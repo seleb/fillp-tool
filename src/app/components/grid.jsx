be3e5ffbd01e3bdfe69a2740f0dacdf9f19ab4c9
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-05-24 12:43:14 -0400

    chore: refactor grid to functional component

--- src/app/components/grid.jsx
+++ src/app/components/grid.jsx
@@ -1,19 +1,17 @@
-import { h, Component } from 'preact';
+import { h } from 'preact';
 import './grid.css';
 import coloursHex from './colours';
 
-export default class Grid extends Component {
-	render({
-		bits = {},
-		onChange,
-		colours,
-	}) {
-		return (
-			<div class="grid">
-				{Object.entries(bits).reverse().map(([bit, on]) => (
-					<div className={`cell ${on ? 'on' : ''}`} style={{ background: coloursHex[colours[on ? 1 : 0]] }} onClick={() => onChange(bit)} />
-				))}
-			</div>
-		);
-	}
+export default function Grid({
+	bits = {},
+	onChange,
+	colours,
+}) {
+	return (
+		<div class="grid">
+			{Object.entries(bits).reverse().map(([bit, on]) => (
+				<div className={`cell ${on ? 'on' : ''}`} style={{ background: coloursHex[colours[on ? 1 : 0]] }} onClick={() => onChange(bit)} />
+			))}
+		</div>
+	);
 }
